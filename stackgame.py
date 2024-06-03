c82c95ad60ef04e4558caa20f83a3de31e654447
donggyunleeeee skkusosc <020315lee@gmail.com>
2024-06-03 16:12:52 +0900

    block_settings

--- stackgame.py
+++ stackgame.py
@@ -4,14 +4,25 @@ import sys
 pygame.init()
 
 size = (800, 600)
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-pygame.display.set_caption("Pygame Test")
+pygame.display.set_caption("Stack Game")
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+block_width = 100
+block_height = 20
+moving_block_y = screen_height // 2
+fixed_block = pygame.Rect((screen_width // 2 - block_width // 2, screen_height - block_height), (block_width, block_height))
+moving_block = pygame.Rect((screen_width // 2 - block_width // 2, moving_block_y), (block_width, block_height))
+
+block_speed = 5
+block_direction = 1
 
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
