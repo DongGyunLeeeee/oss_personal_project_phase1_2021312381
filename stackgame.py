81f19bd740109a0984b255e944563b94e77b643b
donggyunleeeee skkusosc <020315lee@gmail.com>
2024-06-04 14:47:19 +0900

    final_submit

--- stackgame.py
+++ stackgame.py
@@ -63,33 +63,53 @@ class Block:
 
 # 스택 클래스
 class Stack:
+    
+    # 스택 초기화
     def __init__(self):
         self.stack = []
+
+        # 쌓인 블록 수
         self.initSize = 20
+
+        # 초기 블록 세팅
         for i in range(self.initSize):
             newBlock = Block(150, 590 - i * 10, RED, 0)
             self.stack.append(newBlock)
 
+    # 초기 블록 세팅
     def show(self):
         for i in range(self.initSize):
             self.stack[i].draw()
 
+    # 초기 블록 세팅
     def move(self):
         for i in range(self.initSize):
             self.stack[i].move()
 
+    # 블록 추가하기
     def adding(self):
         newBlock = Block(0, 390, RED, speed)
         self.initSize += 1
         self.stack.append(newBlock)
 
+    # 블록 쌓기 게임 진행
     def stacking(self):
+        # 게임 진행 간 유지되어야 하는 블록 너비, 점수 전역 변수 선언
         global block_width, score
+
+        # 맨위 블록 인덱스
         lowerIndex = self.initSize - 2
+
+        # 쌓일 블록 인덱스
         upperIndex = self.initSize - 1
 
+        # 맨위 블록 선언
         lowerBlock = self.stack[lowerIndex]
+
+        # 쌓일 블록 선언
         upperBlock = self.stack[upperIndex]
+
+        # 블록을 미리 쌓은 경우
         if upperBlock.x <= lowerBlock.x and not (upperBlock.x + upperBlock.w < lowerBlock.x):
             self.stack[upperIndex].w = self.stack[upperIndex].x + self.stack[upperIndex].w - lowerBlock.x
             self.stack[upperIndex].x = lowerBlock.x
@@ -97,37 +117,50 @@ class Stack:
                 self.stack[upperIndex].w = lowerBlock.w
             self.stack[upperIndex].speed = 0
             score += 1
+
+        # 블록을 늦게 쌓은 경우
         elif lowerBlock.x <= upperBlock.x <= lowerBlock.x + lowerBlock.w:
             self.stack[upperIndex].w = lowerBlock.x + lowerBlock.w - upperBlock.x
             self.stack[upperIndex].speed = 0
             score += 1
+        
+        # 겹치는 지점이 없게 쌓은 경우
         else:
             ending()
+        
+        # 한 블록 쌓일 때마다 스택 한 블록씩 내리기
         for i in range(self.initSize):
             self.stack[i].y += 10
 
+        # 블록 너비 갱신
         block_width = self.stack[upperIndex].w
 
+# 현재 점수판
 def scoreboard():
     font = pygame.font.SysFont(None, 30)
     text = font.render(str(score), True, WHITE)
     screen.blit(text, (200, 10))
 
+# 최고 점수판
 def highestboard():
     font = pygame.font.SysFont(None, 30)
     text = font.render(str(highest_score), True, WHITE)
     screen.blit(text, (10, 10))
 
+# 엔딩
 def ending():
     global highest_score
     loop = True
 
     font = pygame.font.SysFont(None, 60)
+
+    # 최고점 갱신
     if highest_score < score:
         highest_score = score
         text = font.render("New Record!", True, WHITE)
     else:
         text = font.render("Game Over!", True, WHITE)
+
     textRect = text.get_rect()
     textRect.center = (200, 300)
 
@@ -145,10 +178,12 @@ def ending():
         pygame.display.update()
         clock.tick()
 
+# 파이게임 종료
 def close():
     pygame.quit()
     sys.exit()
 
+# 조작키 설명
 def explain():
     font1 = pygame.font.SysFont(None, 40)
     font2 = pygame.font.SysFont(None, 20)
@@ -172,10 +207,12 @@ def explain():
         clock.tick(60)
 
 
+# 게임 루프
 def game():
     global block_width, block_height, speed, score
     loop = True
 
+    # 세팅 초기화
     block_width = 100
     block_height = 10
     speed = 3
@@ -202,5 +239,6 @@ def game():
         pygame.display.update()
         clock.tick(60)
 
+# 실행
 explain()
 game()
